d71b5d0ac74d39b5b9bfa6dc64897ba5efbff6a2
jandersonvb <jandersonvb.dev@gmail.com>
2024-01-26 14:41:45 -0300

    feat: example of button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
-import { Inter } from "next/font/google";
+import { styled } from "@/styles";
 
-const inter = Inter({ subsets: ["latin"] });
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  color: '#fff',
+  padding: "10px 24px",
+  borderRadius: 5,
+  fontSize: 16,
+  fontWeight: "bold",
+  border: "none",
+  cursor: "pointer",
+  transition: "background-color 0.2s",
+
+  "&:hover": {
+    backgroundColor: "#7723d8",
+  },
+
+
+  span: {
+    fontSize: 20,
+  },
+});
+
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Button>
+        <span>Clique aqui meu Jovem!</span>
+      </Button>
+    </>
   );
 }
+
+
